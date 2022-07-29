598958a9f40f430af82fd259d66123609af50f58
Leon <leonparkin41@outlook.com>
2022-07-29 21:22:25 +0100

    Added a working score counter!

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
+extern crate global_counter;
 
+use global_counter::primitive::exact::CounterI16;
 use rand::Rng;
-use std::io;
 use std::cmp::Ordering;
+use std::io;
 fn main() {
-    let mut score: u8 = 0;
+    let score: u8 = 0;
+    static SCORECNT: CounterI16 = CounterI16::new(0);
 
-    println!("Welcome to my super cool ultra fun guessing game.");
+    println!("Welcome to my super cool ultra fun guessing game. Enter a number from 1-5 and if you are correct, continue on. Good luck!");
 
     loop {
-        println!("Please input your guess.");
+        println!("
+ Please input your guess.");
 
         let mut guess = String::new();
 
@@ -21,21 +25,34 @@ fn main() {
             Err(_) => continue,
         };
 
-        let mut secretnumber = rand::thread_rng().gen_range(1..=5);
+        let secretnumber = rand::thread_rng().gen_range(1..=5);
 
         match guess.cmp(&secretnumber) {
-            Ordering::Equal => println!("You got it!"),
+            Ordering::Equal => {
+                SCORECNT.inc();
+                let score = SCORECNT.get();
+                println!("
+You got it! You're score is {score}");},
             Ordering::Less => {
-                println!("You finished with a score of {score}!
-                The secret number was {secretnumber}");
+                let score = SCORECNT.get();
+                println!("
+ You finished with a score of {score}!
+The secret number was {secretnumber}"
+                );
                 break;
             }
             Ordering::Greater => {
-                println!("You finished with a score of {score}!
-                The secret number was {secretnumber}");
+                let score = SCORECNT.get();
+                println!("
+ You finished with a score of {score}!
+The secret number was {secretnumber}"
+                );
                 break;
             }
         };
-        let score = score + 1;
+    
+    if score == 5 {
+        println!("I give up, you win, you are psychic.");
+    };
     }
 }
